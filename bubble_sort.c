2d0c48f48633f7359f288978d81826f5181d5d64
cheykoff <mail@michael-schoelkopf.de>
2020-10-07 21:40:08 +0200

    Fix algorithm

--- bubble_sort.c
+++ bubble_sort.c
@@ -15,7 +15,7 @@ int main()
 
     bubble_sort_array(array_length, array);
 
-    printf("Sorted array:\n");
+    printf("\nSorted array:\n");
     print_array(array_length, array);
 
     return 0;
@@ -33,17 +33,34 @@ void print_array(int array_length, int array[])
 
 void bubble_sort_array(int array_length, int array[])
 {
+    // Repeat the process of bubbling n - 1 times (max)
     for (int i = 0; i < array_length - 1; i++)
     {
-        for (int j = i + 1; j < array_length; j++)
-        {
-            if (array[i] > array[j])
+        int n = 0;
+        printf("\nThis is the bubbling of the %ith element\n", array_length - i);
+        
+        // Bubbling the last element of the unsorted array
+        for (int j = 0; j < array_length - 1; j++)
+        {   
+            if (array[j] > array[j + 1])
             {
                 // switch elements 
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                printf("This switches the %i (Position %i) and %i (Position %i)\n", array[j], j, array[j + 1], j + 1);
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                n++;
             }
+        }
+        printf("%i arrived at position %i\n", array[array_length - i - 1], array_length - i);
+        printf("The array is now:\n");
+        print_array(array_length, array);
+        
+        // If no switch happens we know that the array is already sorted.
+        if (n == 0)
+        {
+            printf("No more iterations needed. The array is already sorted.\n");
+            return;
         }
     }
     return;
