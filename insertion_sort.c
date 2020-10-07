bed0dee618dcadf0c3278617b980a88abab58a32
cheykoff <mail@michael-schoelkopf.de>
2020-10-07 21:54:28 +0200

    First commit

---
+++ insertion_sort.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Insertion sort\n");
+
+    int array[] = {1, 4, 3, 6, 8, 2, 5, 7};
+
+    // Next: Reuse the print_array function from bubble_sort.c
+
+
+}
